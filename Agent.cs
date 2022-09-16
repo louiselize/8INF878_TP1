968b8d2515c7804112c3ce9187c66b779aebedf8
Louise Lize <louise.lize@etu.utc.fr>
2022-09-17 01:10:17 +0200

    nothing

---
+++ Agent.cs
@@ -0,0 +1,21 @@
+/*
+Agent :
+L’agent Aspirobot T-0.1 possède un certain nombre de fonctionnalités. Il peut aspirer (ce qui a
+pour effet d’aspirer à la fois la poussière et les bijoux), ramasser un élément (pour ramasser un
+bijou idéalement!) et se déplacer (haut, bas, gauche et droite). Chacune de ses actions lui coûte
+1 unité d’électricité (attention, rappelez-vous que vous êtes un propriétaire de manoir cheap
+heum... disons qui n’aime pas gaspiller de l’argent). Le robot peut voir toutes les pièces. Le
+robot doit être implémenté sous les principes agents vus en classe. Il doit donc observer
+l’environnement avec ses capteurs et agir sur celui-ci avec des effecteurs (je vous conseille
+carrément de faire des classes capteurs et effecteurs). Votre robot devrait être de type « basé sur
+les buts ». Même si le problème est très simple, il est important que celui-ci contienne les
+
+éléments de base (son état interne devrait contenir un état mental sous la forme BDI « Beliefs-
+Desires-Intentions »). Il devrait lui aussi être implémenté comme une boucle infinie respectant
+
+l’idée de base vue en cours (voir pseudo-code simple ci-dessous).
+*/
+class Agent
+    {
+        
+    }
